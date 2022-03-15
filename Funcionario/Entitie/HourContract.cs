33eadfa435b1a903afa1e044544d104309b192e2
Matheus Alves <matheus.180960@gmail.com>
2022-03-14 21:47:20 -0300

    Desenvolvendo programa para ler dados de um trabalhador com N contratos

---
+++ Funcionario/Entitie/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Funcionario.Entitie
+{
+    public  class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int  Hours { get; set; }
+
+        public HourContract() 
+        {
+        }
+        
+        public HourContract (DateTime date, double valueperhour,int hours)
+        {
+            Date = date;
+            ValuePerHour = valueperhour;
+            Hours = hours;
+        }
+
+        public double TotalVelue()
+        {
+            return Hours * ValuePerHour;
+        } 
+    }
+}
